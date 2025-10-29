a1364cb2c6e53627f63891dd7275325e8029111e
MathuinLucci <matt.parisano@gmail.com>
2025-10-29 15:03:59 -0500

    finished project

--- LunchOrder/Form1.Designer.cs
+++ LunchOrder/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             gbxMainCourse = new GroupBox();
+            radSalad = new RadioButton();
+            radPizza = new RadioButton();
+            radHamburger = new RadioButton();
             groupBox3 = new GroupBox();
             txtOrderTotal = new TextBox();
             label3 = new Label();
@@ -38,19 +41,65 @@
             label1 = new Label();
             btnPlaceOrder = new Button();
             btnExit = new Button();
+            gbxAddons = new GroupBox();
+            cbxAddon3 = new CheckBox();
+            cbxAddon2 = new CheckBox();
+            cbxAddon1 = new CheckBox();
+            btnClearOrder = new Button();
+            gbxMainCourse.SuspendLayout();
             groupBox3.SuspendLayout();
+            gbxAddons.SuspendLayout();
             SuspendLayout();
             // 
             // gbxMainCourse
             // 
-            gbxMainCourse.Location = new Point(32, 21);
-            gbxMainCourse.Margin = new Padding(2);
+            gbxMainCourse.Controls.Add(radSalad);
+            gbxMainCourse.Controls.Add(radPizza);
+            gbxMainCourse.Controls.Add(radHamburger);
+            gbxMainCourse.Location = new Point(37, 28);
+            gbxMainCourse.Margin = new Padding(2, 3, 2, 3);
             gbxMainCourse.Name = "gbxMainCourse";
-            gbxMainCourse.Padding = new Padding(2);
-            gbxMainCourse.Size = new Size(166, 90);
+            gbxMainCourse.Padding = new Padding(2, 3, 2, 3);
+            gbxMainCourse.Size = new Size(190, 120);
             gbxMainCourse.TabIndex = 0;
             gbxMainCourse.TabStop = false;
             gbxMainCourse.Text = "Main Course";
+            // 
+            // radSalad
+            // 
+            radSalad.AutoSize = true;
+            radSalad.Location = new Point(5, 86);
+            radSalad.Name = "radSalad";
+            radSalad.Size = new Size(116, 24);
+            radSalad.TabIndex = 4;
+            radSalad.TabStop = true;
+            radSalad.Text = "Salad - $6.75";
+            radSalad.UseVisualStyleBackColor = true;
+            radSalad.CheckedChanged += radSalad_CheckedChanged;
+            // 
+            // radPizza
+            // 
+            radPizza.AutoSize = true;
+            radPizza.Location = new Point(5, 56);
+            radPizza.Name = "radPizza";
+            radPizza.Size = new Size(113, 24);
+            radPizza.TabIndex = 1;
+            radPizza.Text = "Pizza - $6.95";
+            radPizza.UseVisualStyleBackColor = true;
+            radPizza.CheckedChanged += radPizza_CheckedChanged;
+            // 
+            // radHamburger
+            // 
+            radHamburger.AutoSize = true;
+            radHamburger.Checked = true;
+            radHamburger.Location = new Point(5, 26);
+            radHamburger.Name = "radHamburger";
+            radHamburger.Size = new Size(155, 24);
+            radHamburger.TabIndex = 0;
+            radHamburger.TabStop = true;
+            radHamburger.Text = "Hamburger - $7.95";
+            radHamburger.UseVisualStyleBackColor = true;
+            radHamburger.CheckedChanged += radHamburger_CheckedChanged;
             // 
             // groupBox3
             // 
@@ -60,116 +109,175 @@
             groupBox3.Controls.Add(label2);
             groupBox3.Controls.Add(txtSubTotal);
             groupBox3.Controls.Add(label1);
-            groupBox3.Location = new Point(32, 139);
-            groupBox3.Margin = new Padding(2);
+            groupBox3.Location = new Point(37, 185);
+            groupBox3.Margin = new Padding(2, 3, 2, 3);
             groupBox3.Name = "groupBox3";
-            groupBox3.Padding = new Padding(2);
-            groupBox3.Size = new Size(250, 107);
+            groupBox3.Padding = new Padding(2, 3, 2, 3);
+            groupBox3.Size = new Size(286, 143);
             groupBox3.TabIndex = 1;
             groupBox3.TabStop = false;
             groupBox3.Text = "Order Total";
             // 
             // txtOrderTotal
             // 
-            txtOrderTotal.Location = new Point(105, 72);
-            txtOrderTotal.Margin = new Padding(2);
+            txtOrderTotal.Location = new Point(120, 96);
+            txtOrderTotal.Margin = new Padding(2, 3, 2, 3);
             txtOrderTotal.Name = "txtOrderTotal";
             txtOrderTotal.ReadOnly = true;
-            txtOrderTotal.Size = new Size(106, 23);
+            txtOrderTotal.Size = new Size(121, 27);
             txtOrderTotal.TabIndex = 5;
             txtOrderTotal.TabStop = false;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(28, 74);
+            label3.Location = new Point(32, 99);
             label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(68, 15);
+            label3.Size = new Size(87, 20);
             label3.TabIndex = 4;
             label3.Text = "Order Total:";
             label3.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // txtSalesTax
             // 
-            txtSalesTax.Location = new Point(105, 46);
-            txtSalesTax.Margin = new Padding(2);
+            txtSalesTax.Location = new Point(120, 61);
+            txtSalesTax.Margin = new Padding(2, 3, 2, 3);
             txtSalesTax.Name = "txtSalesTax";
             txtSalesTax.ReadOnly = true;
-            txtSalesTax.Size = new Size(106, 23);
+            txtSalesTax.Size = new Size(121, 27);
             txtSalesTax.TabIndex = 3;
             txtSalesTax.TabStop = false;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(28, 48);
+            label2.Location = new Point(32, 64);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(69, 15);
+            label2.Size = new Size(86, 20);
             label2.TabIndex = 2;
             label2.Text = "Tax (7.75%):";
             label2.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // txtSubTotal
             // 
-            txtSubTotal.Location = new Point(105, 20);
-            txtSubTotal.Margin = new Padding(2);
+            txtSubTotal.Location = new Point(120, 27);
+            txtSubTotal.Margin = new Padding(2, 3, 2, 3);
             txtSubTotal.Name = "txtSubTotal";
             txtSubTotal.ReadOnly = true;
-            txtSubTotal.Size = new Size(106, 23);
+            txtSubTotal.Size = new Size(121, 27);
             txtSubTotal.TabIndex = 1;
             txtSubTotal.TabStop = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(43, 22);
+            label1.Location = new Point(49, 29);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(54, 15);
+            label1.Size = new Size(68, 20);
             label1.TabIndex = 0;
             label1.Text = "Subtotal:";
             label1.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // btnPlaceOrder
             // 
-            btnPlaceOrder.Location = new Point(318, 146);
-            btnPlaceOrder.Margin = new Padding(2);
+            btnPlaceOrder.Location = new Point(363, 195);
+            btnPlaceOrder.Margin = new Padding(2, 3, 2, 3);
             btnPlaceOrder.Name = "btnPlaceOrder";
-            btnPlaceOrder.Size = new Size(78, 23);
+            btnPlaceOrder.Size = new Size(96, 31);
             btnPlaceOrder.TabIndex = 2;
-            btnPlaceOrder.Text = "Place &Order";
+            btnPlaceOrder.Text = "Place Order";
             btnPlaceOrder.UseVisualStyleBackColor = true;
+            btnPlaceOrder.Click += btnPlaceOrder_Click;
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(318, 223);
-            btnExit.Margin = new Padding(2);
+            btnExit.Location = new Point(363, 297);
+            btnExit.Margin = new Padding(2, 3, 2, 3);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(78, 23);
+            btnExit.Size = new Size(96, 31);
             btnExit.TabIndex = 3;
             btnExit.Text = "E&xit";
             btnExit.UseVisualStyleBackColor = true;
             btnExit.Click += btnExit_Click;
             // 
+            // gbxAddons
+            // 
+            gbxAddons.Controls.Add(cbxAddon3);
+            gbxAddons.Controls.Add(cbxAddon2);
+            gbxAddons.Controls.Add(cbxAddon1);
+            gbxAddons.Location = new Point(246, 28);
+            gbxAddons.Name = "gbxAddons";
+            gbxAddons.Size = new Size(250, 120);
+            gbxAddons.TabIndex = 4;
+            gbxAddons.TabStop = false;
+            gbxAddons.Text = "Add-on Items ($1.25/each)";
+            // 
+            // cbxAddon3
+            // 
+            cbxAddon3.AutoSize = true;
+            cbxAddon3.Location = new Point(6, 83);
+            cbxAddon3.Name = "cbxAddon3";
+            cbxAddon3.Size = new Size(108, 24);
+            cbxAddon3.TabIndex = 2;
+            cbxAddon3.Text = "French Fries";
+            cbxAddon3.UseVisualStyleBackColor = true;
+            // 
+            // cbxAddon2
+            // 
+            cbxAddon2.AutoSize = true;
+            cbxAddon2.Location = new Point(6, 53);
+            cbxAddon2.Name = "cbxAddon2";
+            cbxAddon2.Size = new Size(190, 24);
+            cbxAddon2.TabIndex = 1;
+            cbxAddon2.Text = "Ketchup, mustard, mayo";
+            cbxAddon2.UseVisualStyleBackColor = true;
+            // 
+            // cbxAddon1
+            // 
+            cbxAddon1.AutoSize = true;
+            cbxAddon1.Location = new Point(6, 23);
+            cbxAddon1.Name = "cbxAddon1";
+            cbxAddon1.Size = new Size(180, 24);
+            cbxAddon1.TabIndex = 0;
+            cbxAddon1.Text = "Lettuce, tomato, onion";
+            cbxAddon1.UseVisualStyleBackColor = true;
+            // 
+            // btnClearOrder
+            // 
+            btnClearOrder.Location = new Point(363, 244);
+            btnClearOrder.Name = "btnClearOrder";
+            btnClearOrder.Size = new Size(94, 29);
+            btnClearOrder.TabIndex = 5;
+            btnClearOrder.Text = "Clear Order";
+            btnClearOrder.UseVisualStyleBackColor = true;
+            btnClearOrder.Click += btnClearOrder_Click;
+            // 
             // Form1
             // 
             AcceptButton = btnPlaceOrder;
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             CancelButton = btnExit;
-            ClientSize = new Size(452, 270);
+            ClientSize = new Size(517, 360);
+            Controls.Add(btnClearOrder);
+            Controls.Add(gbxAddons);
             Controls.Add(btnExit);
             Controls.Add(btnPlaceOrder);
             Controls.Add(groupBox3);
             Controls.Add(gbxMainCourse);
-            Margin = new Padding(2);
+            Margin = new Padding(2, 3, 2, 3);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Lunch Order";
+            gbxMainCourse.ResumeLayout(false);
+            gbxMainCourse.PerformLayout();
             groupBox3.ResumeLayout(false);
             groupBox3.PerformLayout();
+            gbxAddons.ResumeLayout(false);
+            gbxAddons.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -185,5 +293,13 @@
         private Label label1;
         private Button btnPlaceOrder;
         private Button btnExit;
+        private RadioButton radPizza;
+        private RadioButton radHamburger;
+        private RadioButton radSalad;
+        private GroupBox gbxAddons;
+        private CheckBox cbxAddon2;
+        private CheckBox cbxAddon1;
+        private CheckBox cbxAddon3;
+        private Button btnClearOrder;
     }
 }
--- LunchOrder/Form1.cs
+++ LunchOrder/Form1.cs
@@ -5,11 +5,165 @@ namespace LunchOrder
         public Form1()
         {
             InitializeComponent();
+            this.ControlBox = false;
+            this.AcceptButton = btnPlaceOrder;
+            this.CancelButton = btnExit;
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        public string[] addon = new string[]
+        {
+            "Lettuce, tomato, onion", "Ketchup, mustard, mayo", "French Fries",
+            "Pepperoni", "Sausage", "Olives",
+            "Crutons", "Bacon Bits", "Bread Sticks"
+        };
+
+        public string[] priceText = new string[]
+        {
+            "Add-on Items ($1.25/each)", "Add-on Items ($0.75/each)", "Add-on Items ($0.50/each)"
+        };
+
+        public double[] addonPrice = new double[]
+        {
+            1.25d, 0.75d, 0.50d
+        };
+
+        public double[] mainPrice = new double[]
+        {
+            7.95d,6.95d, 6.75d
+        };
+
+        public double mainCost;
+        public double addonCost;
+        public double subtotal;
+        public double tax;
+        public double orderTotal;
+
+        void ClearTotals()
+        {
+            txtSubTotal.Text = "";
+            txtSalesTax.Text = "";
+            txtOrderTotal.Text = "";
+            addonCost = 0;
+            subtotal = 0;
+        }
+
+        void ClearAddOns()
+        {
+            cbxAddon1.Checked = false;
+            cbxAddon2.Checked = false;
+            cbxAddon3.Checked = false;
+        }
+
+        public void radHamburger_CheckedChanged(object sender, EventArgs e)
+        {
+            this.ClearAddOns();
+            cbxAddon1.Text = addon[0];
+            cbxAddon2.Text = addon[1];
+            cbxAddon3.Text = addon[2];
+            gbxAddons.Text = priceText[0];
+        }
+
+        private void radPizza_CheckedChanged(object sender, EventArgs e)
+        {
+            this.ClearAddOns();
+            cbxAddon1.Text = addon[3];
+            cbxAddon2.Text = addon[4];
+            cbxAddon3.Text = addon[5];
+            gbxAddons.Text = priceText[1];
+        }
+
+        private void radSalad_CheckedChanged(object sender, EventArgs e)
+        {
+            this.ClearAddOns();
+            cbxAddon1.Text = addon[6];
+            cbxAddon2.Text = addon[7];
+            cbxAddon3.Text = addon[8];
+            gbxAddons.Text = priceText[2];
+        }
+
+        private void btnPlaceOrder_Click(object sender, EventArgs e)
+        {
+
+            if (radHamburger.Checked)
+            {
+                if (cbxAddon1.Checked)
+                {
+                    subtotal += addonPrice[0];
+                }
+
+                if (cbxAddon2.Checked)
+                {
+                    subtotal += addonPrice[0];
+                }
+
+                if (cbxAddon3.Checked)
+                {
+                    subtotal += addonPrice[0];
+                }
+
+                subtotal += mainPrice[0];
+
+            }
+            if (radPizza.Checked)
+            {
+                if (cbxAddon1.Checked)
+                {
+                    subtotal += addonPrice[1];
+                }
+
+                if (cbxAddon2.Checked)
+                {
+                    subtotal += addonPrice[1];
+                }
+
+                if (cbxAddon3.Checked)
+                {
+                    subtotal += addonPrice[1];
+                }
+
+                subtotal += mainPrice[1];
+
+            }
+
+            if (radSalad.Checked)
+            {
+                if (cbxAddon1.Checked)
+                {
+                    subtotal += addonPrice[2];
+                }
+
+                if (cbxAddon2.Checked)
+                {
+                    subtotal += addonPrice[2];
+                }
+
+                if (cbxAddon3.Checked)
+                {
+                    subtotal += addonPrice[2];
+                }
+                subtotal += mainPrice[2];
+            }
+
+            subtotal = Math.Round(subtotal, 2);
+            tax = subtotal * 0.0775;
+            tax = Math.Round(tax, 2);
+            orderTotal = Math.Round(subtotal + tax, 2);
+            txtSubTotal.Text = subtotal.ToString();
+            txtSalesTax.Text = tax.ToString();
+            txtOrderTotal.Text = orderTotal.ToString();
+
+        }
+
+        private void btnClearOrder_Click(object sender, EventArgs e)
+        {
+            radHamburger.Checked = true;
+            ClearAddOns();
+            ClearTotals();
         }
     }
 }
